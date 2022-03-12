338fc855e35c99c9d89c926dccf575ec9cdb0431
Amos Han <ah847@scarletmail.rutgers.edu>
2022-03-12 12:45:29 -0500

    renamed more files

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
-const User = require('./User');
-const Post = require('./Post');
-const Comment = require('./Comment');
+const User = require('./user.js');
+const Post = require('./post.js');
+const Comment = require('./comment.js');
 
 //associations
 User.hasMany(Post, {
