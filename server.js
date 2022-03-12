6e3a0231db323c87810f92b481c75861797c79a9
Amos Han <ah847@scarletmail.rutgers.edu>
2022-03-12 12:42:20 -0500

    renamed controller on server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const routes = require('./controllers');
+const routes = require('./controller');
 const sequelize = require('./config/connection');
 const path = require('path');
 
